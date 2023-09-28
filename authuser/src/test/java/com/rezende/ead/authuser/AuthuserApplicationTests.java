751808bd5da42d5f07dca87d735e79f86ec02eea
andremrezende-ecore <andre.rezende@e-core.com>
2023-09-28 09:38:28 -0300

    Added new dependencies

---
+++ authuser/src/test/java/com/rezende/ead/authuser/AuthuserApplicationTests.java
@@ -0,0 +1,11 @@
+package com.rezende.ead.authuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthuserApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
